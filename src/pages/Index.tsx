0c81e508e8100977efb37be41e5aac081bfd8d1d
Michel Fernandes <michelpf@gmail.com>
2026-01-07 17:52:57 -0500

    Fix typo in VALID_IDS array

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,6 +1,6 @@
 import { useState, useRef, useEffect } from "react";
 
-const VALID_IDS = ["12345678", "PATIENT01", "TEST2024", "SKEANUR"];
+const VALID_IDS = ["12345678", "PATIENT01", "TEST2024", "SKEANURE"];
 
 const Index = () => {
   const [step, setStep] = useState(1);
